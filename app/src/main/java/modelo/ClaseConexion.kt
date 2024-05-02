1f600a10d59594007709c333fbd0b6ec10004689
JoseIraheta234 <Jluisiraheta097@gmail.com>
2024-05-02 15:39:18 -0600

    soy fito

---
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -0,0 +1,19 @@
+package modelo
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+class ClaseConexion {
+    fun cadenaConexion(): Connection?{
+        try {
+            val ip = "jdbc:oracle:thin:@10.10.0.146:1521:xe"
+            val usuario = "system"
+            val contrasena = "desarrollo"
+            val conexion = DriverManager.getConnection(ip,usuario,contrasena)
+            return conexion
+        }catch (e: Exception){
+            println("El error es este: $e")
+            return null
+        }
+    }
+}
